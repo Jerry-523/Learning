b166e5c6afb4c3c5604c7c8a7edcdb2d26789f30
Péricles Borges <92488227+Jerry-523@users.noreply.github.com>
2024-10-17 11:58:21 +0000

    Create MeuPrimeiroContrato.sol

---
+++ Blockchain/Solidity/MeuPrimeiroContrato.sol
@@ -0,0 +1,9 @@
+// SPDX-License-identifirt: MIT
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MeuPrimeiroContrato {
+    function olaMundo() public pure returns(string memory) {
+        return "Ola, mundo!";
+    }
+}
